6ef38a6fc912995e1a19d23aaafb244f30d35b63
Amadeusz Wieczorek <git@amadeusw.com>
2016-03-30 09:15:51 -0700

    Adds intro demos

---
+++ AliveDemos/AliveDemos.Intro1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliveDemos.Intro1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var greeting = $"Hello {args[0]}";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            const double SLICES_PER_PIZZA = 8.0;
+
+            var desiredSlices = new[] { 0 };
+            var developers = desiredSlices.Count();
+            var totalDesiredSlices = desiredSlices.Sum();
+
+            var pizzas = Math.Ceiling(totalDesiredSlices / SLICES_PER_PIZZA);
+
+            var totalSlices = pizzas * SLICES_PER_PIZZA;
+            var averageSlicesPerDev = totalSlices / developers;
+
+            var result = $"Average {averageSlicesPerDev:f1} slices per dev";
+
+
+        }
+
+
+        private void sample()
+        {
+            var zero = 0;
+            var one = 1;
+            try
+            {
+                var surprise = one / zero;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ AliveDemos/AliveDemos.Intro2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AliveDemos.Intro2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var greeting = $"Hello {args[0]}";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            var tenures = new[] { .5, 2, 1, 1.5, 2, 1, 1 };
+            var developers = tenures.Count();
+
+            var averageTenure = tenures.Average();
+
+            var result = $"Average tenure: {averageTenure:f1}";
+
+            foreach (var developersTenure in tenures)
+            {
+                var delta = developersTenure - averageTenure;
+                var deltaString = delta.ToString("f1");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void sample()
+        {
+            var zero = 0;
+            var one = 1;
+            try
+            {
+                var surprise = one / zero;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
